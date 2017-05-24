da24a0e8a6eca171844886e721db66008b47b68a
okaikov <oren.kaikov@gmail.com>
2017-05-24 21:52:27 +0300

    partially solved the duplicate edges

--- pin-3.2-81205-gcc-linux/source/tools/ex2/ex2.cpp
+++ pin-3.2-81205-gcc-linux/source/tools/ex2/ex2.cpp
@@ -282,6 +282,7 @@ public:
                 edge_right);
             if (fit == tmp_rtn_obj.edges.end()) {
                 tmp_rtn_obj.edges.push_back(edge_right);
+				break;
             } else {
                 *fit += edge_right;
             }
