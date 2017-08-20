bd111bf19d049a936b59e050bc92ffca95cf8231
BARKer <bawplayer@gmail.com>
2017-08-20 11:44:16 +0300

    Insert malloc instrumentation. Pre-routine only. Post doesn't work in probe mode, yet.

--- pin-3.2-81205-gcc-linux/source/tools/project/project.cpp
+++ pin-3.2-81205-gcc-linux/source/tools/project/project.cpp
@@ -74,7 +74,269 @@ KNOB<BOOL>   KnobDumpTranslatedCode(KNOB_MODE_WRITEONCE,    "pintool",
 KNOB<BOOL>   KnobDoNotCommitTranslatedCode(KNOB_MODE_WRITEONCE,    "pintool",
     "no_tc_commit", "0", "Do not commit translated code");
 
+/* ===================================================================== */
 
+#include <set>
+#include "Tracer.h"
+
+using std::set;
+
+/* ===================================================================== */
+/* Names of malloc and free */
+/* ===================================================================== */
+#define MALLOC "malloc"
+#define FREE "free"
+#define MAIN "main"
+
+/* ===================================================================== */
+/* Global Variables */
+/* ===================================================================== */
+
+std::ostream& TraceFile = std::cout;
+bool mainInit = false;
+bool mainFinished = false;
+ADDRINT lastMallocSize;
+Tracer mallocTracer = Tracer::GetInstance();
+set<ADDRINT> suspiciousAddresses;
+
+/* ===================================================================== */
+
+bool IsCalledAfterMain()
+{
+	// Don't run instumentation code unless main has started
+	if (!mainInit || mainFinished)
+		return false;
+	
+	return true;
+}
+
+/* ===================================================================== */
+/* Analysis routines                                                     */
+/* ===================================================================== */
+ 
+VOID Arg1Before(CHAR * name, ADDRINT size)
+{
+	if (!IsCalledAfterMain())
+		return;
+
+	lastMallocSize = size;
+}
+
+VOID AfterFree(CHAR * name, ADDRINT addr)
+{
+	if (!IsCalledAfterMain())
+		return;
+
+	mallocTracer.DeleteAddress(addr);
+}
+
+VOID MallocAfter(ADDRINT ret)
+{
+	if (!IsCalledAfterMain())
+		return;
+	
+	mallocTracer.AddNewAddress(ret, lastMallocSize);
+}
+
+VOID mainBefore()
+{
+	mainInit = true;
+}
+
+VOID mainAfter()
+{
+	mainFinished =  true;
+}
+
+// Print a memory read record
+VOID RecordMemRead(VOID * ip, ADDRINT addr)
+{
+	if (!IsCalledAfterMain())
+		return;
+	
+	if (suspiciousAddresses.count((ADDRINT)ip) !=0)
+		cout << "Memory read overflow at address: 0x" << hex << (ADDRINT)ip << dec << endl;
+}
+
+// Print a memory write record
+VOID RecordMemWrite(VOID* ip, ADDRINT addr)
+{
+	if (!IsCalledAfterMain())
+		return;
+	
+	if (suspiciousAddresses.count((ADDRINT)ip) !=0)
+		cout << "Memory write overflow at address: 0x" << hex << (ADDRINT)ip << dec << endl;
+}
+
+VOID CheckAddIns(ADDRINT regVal, UINT64 immediate, VOID* ip, UINT64 insSize)
+{
+	if (!mallocTracer.IsAllocatedAddress(regVal))
+		return;
+
+	if (mallocTracer.GetStartAddress(regVal + immediate) != mallocTracer.GetStartAddress(regVal))
+		suspiciousAddresses.insert(ADDRINT(ip) + insSize);
+}
+
+bool INS_IsAdd(INS ins)
+{
+	string insDisassembly = INS_Disassemble(ins);
+	if (insDisassembly.substr(0, 3) == "add")
+		return true;
+	
+	return false;
+}
+
+VOID CheckAddInsIndexReg(ADDRINT regVal, ADDRINT indexRegVal, VOID* ip, UINT64 insSize)
+{
+	if (!mallocTracer.IsAllocatedAddress(regVal))
+		return;
+		
+	if (mallocTracer.GetStartAddress(regVal + indexRegVal) != mallocTracer.GetStartAddress(regVal))
+		suspiciousAddresses.insert(ADDRINT(ip) + insSize);
+}
+
+/* ===================================================================== */
+/* Instrumentation routines                                              */
+/* ===================================================================== */
+   
+VOID mallocImage(IMG img)
+{
+    // Instrument the malloc() and free() functions.  Print the input argument
+    // of each malloc() or free(), and the return value of malloc().
+    
+    //  Find the malloc() function.
+    RTN mallocRtn = RTN_FindByName(img, MALLOC);
+    if (RTN_Valid(mallocRtn)) {
+        // RTN_Open(mallocRtn);
+
+        RTN_InsertCallProbed(mallocRtn, IPOINT_BEFORE, (AFUNPTR)Arg1Before,
+                       IARG_ADDRINT, MALLOC,
+                       IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+                       IARG_END);
+                       
+/*        RTN_InsertCallProbed(mallocRtn, IPOINT_AFTER, (AFUNPTR)MallocAfter,
+                       IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+*/
+        // RTN_Close(mallocRtn);
+    }
+    // Find the free() function.
+/*    RTN freeRtn = RTN_FindByName(img, FREE);
+    if (RTN_Valid(freeRtn))
+    {
+        RTN_Open(freeRtn);
+        // Instrument free()
+        RTN_InsertCallProbed(freeRtn, IPOINT_AFTER, (AFUNPTR)AfterFree,
+                       IARG_ADDRINT, FREE,
+                       IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
+                       IARG_END);
+        RTN_Close(freeRtn);
+    }*/
+    
+    RTN mainRtn = RTN_FindByName(img, MAIN);
+	if (RTN_Valid(mainRtn))
+	{
+		// RTN_Open(mainRtn);
+		
+		RTN_InsertCallProbed(mainRtn, IPOINT_BEFORE, (AFUNPTR)mainBefore, IARG_END);
+		
+		// RTN_InsertCallProbed(mainRtn, IPOINT_AFTER, (AFUNPTR)mainAfter, IARG_END);
+		
+		// RTN_Close(mainRtn);
+	}
+}
+
+// Pin calls this function every time a new rtn is executed
+VOID Routine(RTN rtn, VOID *v)
+{
+    RTN_Open(rtn);
+    // For each instruction of the routine
+    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
+    {
+		if (INS_IsAdd(ins))
+		{
+			UINT32 opNum = INS_OperandCount(ins);
+			UINT64 immediate = 0;
+			REG operandReg = REG_INVALID();
+			REG indexReg = REG_INVALID();
+			bool foundReg = false;
+			bool foundIndexReg = false;
+			bool foundImm = false;
+
+			for (UINT32 i = 0; i < opNum; ++i)
+			{
+				if (!foundImm && INS_OperandIsImmediate(ins, i))
+				{
+					immediate = INS_OperandImmediate(ins, i);
+					foundImm = true;
+				}
+				
+				else if (!foundReg && INS_OperandIsReg(ins, i) && INS_OperandWritten(ins, i))
+				{
+					operandReg = INS_OperandReg(ins, i);
+					if (REG_INVALID() != operandReg )
+						foundReg = true;
+				}
+				
+				else if (!foundIndexReg && INS_OperandIsReg(ins, i) && INS_OperandReadOnly(ins, i))
+				{
+					indexReg = INS_OperandReg(ins, i);
+					if (REG_INVALID() != indexReg)
+						foundIndexReg = true;
+				}
+
+				if (foundReg && foundImm && REG_valid_for_iarg_reg_value(operandReg))
+				{
+					INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)CheckAddIns, 
+						IARG_REG_VALUE, operandReg, IARG_UINT64, immediate,
+						IARG_INST_PTR, IARG_UINT64, INS_Size(ins), IARG_END);
+					break;
+				}
+				
+				else if (foundIndexReg && foundReg && REG_valid_for_iarg_reg_value(operandReg)
+					&& REG_valid_for_iarg_reg_value(indexReg))
+				{
+					INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)CheckAddInsIndexReg, 
+						IARG_REG_VALUE, operandReg, IARG_REG_VALUE, indexReg,
+						IARG_INST_PTR, IARG_UINT64, INS_Size(ins), IARG_END);
+					break;
+				}
+			}
+		} 
+		else 
+		{
+			UINT32 memOperands = INS_MemoryOperandCount(ins);
+
+			// Iterate over each memory operand of the instruction.
+			for (UINT32 memOp = 0; memOp < memOperands; memOp++)
+			{
+				if (INS_MemoryOperandIsRead(ins, memOp))
+				{
+					 INS_InsertCall(
+						ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
+						IARG_INST_PTR,
+						IARG_MEMORYOP_EA, memOp,
+						IARG_END);
+				}
+				// Note that in some architectures a single memory operand can be 
+				// both read and written (for instance incl (%eax) on IA-32)
+				// In that case we instrument it once for read and once for write.
+				if (INS_MemoryOperandIsWritten(ins, memOp))
+				{
+
+					 INS_InsertCall(
+						ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
+						IARG_INST_PTR,
+						IARG_MEMORYOP_EA, memOp,
+						IARG_UINT64, INS_Size(ins),
+						IARG_END);
+				}
+			}
+		}
+    }
+    RTN_Close(rtn);
+}
+
+/* ======================================================================= */
 
 /* ===================================================================== */
 /* Global Variables */
@@ -1219,7 +1481,6 @@ int allocate_and_init_memory(IMG img)
 	return 0;
 }
 
-
 /* ============================================ */
 /* Main translation routine                     */
 /* ============================================ */
@@ -1228,9 +1489,12 @@ VOID ImageLoad(IMG img, VOID *v)
 	// debug print of all images' instructions
 	//dump_all_image_instrs(img);
 
+	mallocImage(img);
+
     // Step 0: Check that the image is of the main executable file:
-	if (!IMG_IsMainExecutable(img))
+	if (!IMG_IsMainExecutable(img)) {
 		return;
+	}
 
 	int rc = 0;
 
