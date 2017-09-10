09301e018ea9dd24e7c1ef500001f119cf9e8c67
BARKer <bawplayer@gmail.com>
2017-09-10 13:06:16 +0300

    Remove redundant insert_call_probed

--- pin-3.2-81205-gcc-linux/source/tools/project/project.cpp
+++ pin-3.2-81205-gcc-linux/source/tools/project/project.cpp
@@ -221,11 +221,6 @@ VOID mallocImage(IMG img) {
     //  Find the malloc() function.
     RTN mallocRtn = RTN_FindByName(img, MALLOC);
     if (RTN_Valid(mallocRtn)) {
-        RTN_InsertCallProbed(mallocRtn, IPOINT_BEFORE, (AFUNPTR)Arg1Before,
-                       IARG_ADDRINT, MALLOC,
-                       IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
-                       IARG_END);
-
         PROTO proto_malloc = PROTO_Allocate( PIN_PARG(void *), CALLINGSTD_DEFAULT,
             MALLOC, PIN_PARG(int), PIN_PARG_END() );
                        
@@ -249,9 +244,7 @@ VOID mallocImage(IMG img) {
     }
     
     RTN mainRtn = RTN_FindByName(img, MAIN);
-	if (RTN_Valid(mainRtn)) {
-		RTN_InsertCallProbed(mainRtn, IPOINT_BEFORE, (AFUNPTR)mainBefore, IARG_END);
-		
+	if (RTN_Valid(mainRtn)) {		
 		PROTO proto_main = PROTO_Allocate( PIN_PARG(int), CALLINGSTD_DEFAULT,
             FREE, PIN_PARG(int), PIN_PARG(char*), PIN_PARG_END() );
 		
